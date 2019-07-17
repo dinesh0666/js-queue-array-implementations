d6d7258f3511d7529ce5d15d9a7fa15128d95e77
dinesh0666 <dineshkumar06@yahoo.com>
2019-07-17 11:03:33 +0530

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Queue Implementation using Array</h1>`;
+
+var letMeEat = ['Breakfast'];
+var count = letMeEat.push('Lunch','Dinner');
+console.log(count);
